599528efa1668260bfe4296cfa08fdd81ce64957
vishnu <gvvishnupriyen@gmail.com>
2026-08-03 19:15:47 +0530

    did (-1 -1) range for different features ad it failed

--- nl_rc_2/nl_ngrc_-1_1_features/var.py
+++ nl_rc_2/nl_ngrc_-1_1_features/var.py
@@ -1,21 +1,77 @@
 import numpy as np
 import random
 from scipy import linalg
-from sklearn.preprocessing import PolynomialFeatures
+from sklearn.preprocessing import MinMaxScaler, PolynomialFeatures
 from sklearn.metrics import mean_squared_error
 from sklearn.preprocessing import StandardScaler
 import matplotlib.pyplot as plt
 
 class nlfea:
-    def __init__(self,b=0.5,n=10,test_len=10,reg=1e-8,k=3,degree=3):
+    def __init__(self,b=0.499,n=10,test_len=10,reg=1e-8,k=3,degree=3):
         self.b = b
         self.n = n
         self.test_len = test_len
         self.reg = reg
         self.k = k
         self.degree = degree
-        self.feature_scalar = StandardScaler()
         self.insize = 3
+
+    def ss_to_binary(self,x,thres):
+        return (np.array(x)>thres).astype(int)
+
+    def firing_rate(self,x):
+        b = self.b
+        c = 0
+        for v in x:
+            if v>b:
+                c+=1
+        return c/len(x)
+    
+    def variance(self,x):
+        if len(x) ==0:
+            return 0
+        return np.var(x)
+    
+    def energy(self,x):
+        if len(x) ==0:
+            return 0
+        return np.mean(np.square(x))
+    
+    def entropy(self,x):
+        b = self.b
+        if len(x) ==0:
+            return 0
+        x = self.ss_to_binary(x,b)
+        p = np.count_nonzero(x)/len(x)
+        eps = 1e-10
+        p = np.clip(p,eps,1-eps)
+        return -(p*np.log2(p)) - ((1-p)*np.log2(1-p))
+    
+    def sigmoid(self,x):
+        return 1/(1+np.exp(-x))
+    
+    def softsign(self,x):
+        return 0.5*(x/(1+np.abs(x)) + 1)
+    
+    def atan01(self,x):
+        return np.arctan(x)/np.pi + 0.5
+    
+    def neuron_gen(self,x):
+        b = self.b
+        neigh = 1e-8
+        x = np.clip(x,neigh,1-neigh)
+
+        if x>=b:
+            return (1-x)/(1-b)
+        return x/b
+    
+    def neuron_iterator(self,u):
+        n = self.n
+        X = np.zeros(n)
+        X[0] = u.item()
+        for j in range(n-1):
+            X[j+1] = self.neuron_gen(X[j])
+        return X
     
     def build_features(self,data,delay_X_train):
         k = self.k
@@ -23,13 +79,20 @@ class nlfea:
         insize = self.insize
         delay_buffer = delay_X_train.copy()
         self.poly = PolynomialFeatures(degree=deg)
-        dummy_lin = np.zeros(insize*(k+1))
+        dummy_lin = np.zeros(insize*2*(k+1))
         dummy_poly = self.poly.fit_transform(dummy_lin.reshape(1,-1))
         self.feat_size = dummy_poly.shape[1]
 
         history_x = np.zeros((self.feat_size,len(data)))
         for i in range(len(data)):
-            lin = np.concatenate((delay_buffer.flatten(),data[i]))
+            lin = np.zeros(insize*2*(k+1))
+            search_span = np.concatenate((delay_buffer.flatten(),data[i]))
+            for j in range(len(search_span)):
+                u = search_span[j]
+                X = self.neuron_iterator(abs(u))
+                var = self.variance(X)
+                lin[2*j+0] = u
+                lin[2*j+1] = var
             delay_buffer = np.vstack((delay_buffer[1:],data[i]))
             poly_feat = self.poly.transform(lin.reshape(1,-1))
             history_x[:,i] = poly_feat.flatten()
@@ -51,10 +114,17 @@ class nlfea:
         Y = np.zeros((3,test_len))
         delay_buffer = delay_X_test.copy()
         for i in range(test_len):
-            lin = np.concatenate((delay_buffer.flatten(),u))
+            lin = np.zeros(insize*2*(k+1))
+            search_span = np.concatenate((delay_buffer.flatten(),u))
+            for j in range(len(search_span)):
+                uj = search_span[j]
+                X = self.neuron_iterator(abs(uj))
+                var = self.variance(X)
+                lin[2*j+0] = uj
+                lin[2*j+1] = var
             poly_feat = self.poly.transform(lin.reshape(1,-1))
             y = np.dot(self.w_out,poly_feat.flatten())
-            y = np.clip(y,-3,3)
+            y = np.clip(y,-1,1)
             Y[:, i] = y
             delay_buffer = np.vstack((delay_buffer[1:],u))
             u = y
@@ -168,47 +238,12 @@ def generate_rossler(
 
     return data
 
-def generate_rossler(
-    t_max=500,
-    dt=0.01,
-    initial_state=(1.0, 1.0, 1.0),
-    a=0.2,
-    b=0.2,
-    c=5.7,
-    discard=5000
-):
-    def rossler(t, state):
-        x, y, z = state
-
-        dxdt = -y - z
-        dydt = x + a * y
-        dzdt = b + z * (x - c)
-
-        return [dxdt, dydt, dzdt]
-
-    t_eval = np.arange(0, t_max, dt)
-
-    sol = solve_ivp(
-        rossler,
-        (0, t_max),
-        initial_state,
-        t_eval=t_eval,
-        method="RK45"
-    )
-
-    data = sol.y.T
-
-    if discard > 0:
-        data = data[discard:]
-
-    return data
-
-scalar = StandardScaler()
+scalar = MinMaxScaler(feature_range=(-1,1))
 data = generate_lorenz()
 data = data[1000:]
 train_len = 3000
 test_len = 500
-k = 9
+k = 3
 
 p = 0
 push = train_len+p+k
@@ -227,7 +262,8 @@ y_train = scalar.transform(y_train)
 X_test = scalar.transform(X_test)
 delay_X_test = scalar.transform(delay_X_test)
 
-model = nlfea(test_len=test_len,degree=2,k=k,n=4,reg=1e-4)
+
+model = nlfea(test_len=test_len,degree=2,k=k,n=3,reg=1e-5)
 model.fit(X_train,y_train,delay_X_train)
 y_pred = model.predict(X_test[0],delay_X_test).T
 y_pred = scalar.inverse_transform(y_pred)
@@ -242,5 +278,3 @@ plt.plot(np.arange(len(y_test)),y_test,c='r',label='real')
 plt.plot(np.arange(len(y_pred)),y_pred,c='b',label='predicted')
 plt.legend()
 plt.show()
-
-
